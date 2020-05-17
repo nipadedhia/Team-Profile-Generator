6912a0de8a20f8d803fb992d9d1d11be17bcb0ba
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-05-17 13:40:28 -0500

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -57,7 +56,7 @@ function newEmployee() {
               response.name,
               response.id,
               response.email,
-              internData.school
+              internData.school,
             );
             employeeArray.push(intern);
             addEmployee();
@@ -78,7 +77,7 @@ function newEmployee() {
               response.name,
               response.id,
               response.email,
-              engineerData.github
+              engineerData.github,
             );
             employeeArray.push(engineer);
             addEmployee();
@@ -99,7 +98,7 @@ function newEmployee() {
               response.name,
               response.id,
               response.email,
-              managerData.officeNumber
+              managerData.officeNumber,
             );
             employeeArray.push(manager);
             addEmployee();
